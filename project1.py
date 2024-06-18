9a1861572866585bf1d823b63a30ca94680cdb13
jgonzales2211 <118328575+jgonzales2211@users.noreply.github.com>
2024-06-18 01:05:02 -0700

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# display a title
+print ("Employee Management System")
+print("------------------------------------")
+print ("Your Information")
+print("-----------------")
+
+#get input from the user
+first_name= input("Enter your First Name: ")
+last_name= input("Enter your Last Name: ")
+company= input("Enter your Company Name: ")
+street_address= input("Enter your Street Address: ")
+city= input("Enter your City: ")
+state= input("Enter your State: ")
+zip_code= input("Enter your Zip Code: ")
+hire_year= input("Enter your Hire Year: ")
+
+#display a title
+print("Your Profile")
+print("-----------------")
+
+#display the result
+print()
+print(last_name + " , " + first_name )
+print(company)
+print(street_address)
+print(city + " , " + state + " " + zip_code)
+print("Your temporary password: " + Mark*London%2010)
+print("Congratulations! Your profile has been created.")
+print("--------------------------------")
+print("Bye!")
